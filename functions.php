54b2e33099f507e135d5092e0b04ba2a7cfef394
Tim Spinks <tim@ninthlink.com>
2015-02-27 16:36:30 -0800

    updated for fix

--- functions.php
+++ functions.php
@@ -271,8 +271,3 @@ function GetTubContent(){
 		die();
 }
 
-
-//fix for cookie error while login.
-setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN);
-if ( SITECOOKIEPATH != COOKIEPATH )
-	setcookie(TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN);
